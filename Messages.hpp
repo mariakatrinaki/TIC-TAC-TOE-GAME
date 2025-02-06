764e68f39809f3f48f598c9e09134edea7dc1fb8
mariakatrinaki <81016499+mariakatrinaki@users.noreply.github.com>
2025-02-06 11:49:49 +0200

    Add files via upload

---
+++ Messages.hpp
@@ -0,0 +1,16 @@
+#ifndef MESSAGES_HPP
+#define MESSAGES_HPP
+#include "Player.hpp"
+
+
+vector<Player> menu();
+
+vector<Player> players();
+
+bool validName(string name);
+
+void invalidInput();
+
+
+
+#endif // GAME_HPP
